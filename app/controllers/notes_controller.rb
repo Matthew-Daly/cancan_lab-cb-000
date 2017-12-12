c3dcc69db6663c38e27ef1231eea960761e3a593
Matthew <github email address>
2017-12-12 21:37:47 +0700

    Done.

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,49 @@
+class NotesController < ApplicationController
+ 
+  load_and_authorize_resource only: [:edit, :show, :update]
+
+  def index
+    @notes = Note.none
+    if current_user 
+      @notes = current_user.readable
+    end
+  end
+  
+  def new
+    render partial: 'form', locals: { note: Note.new}
+  end
+
+  def create
+    if current_user
+      note = Note.new(note_params)
+      note.user = current_user
+      note.save!
+    end
+    redirect_to '/'
+  end
+
+  # def update
+  #   if current_user
+  #     @note.update(note_params)
+  #   end
+  #   redirect_to '/'
+  # end
+
+  def update
+    @note.update(note_params)
+    redirect_to '/' 
+  end
+
+  def edit
+  end
+
+  def show
+  end
+
+  
+
+  private
+    def note_params
+      params.require(:note).permit(:content, :visible_to)
+    end
+end
